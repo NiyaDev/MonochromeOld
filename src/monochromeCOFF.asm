55b721b584303e9d2fb194e80dec71a4d0c23d4c
szyfr <jmanz5@hotmail.com>
2021-05-22 01:48:38 -0400

    testing compiling as COFF for linking with gcc over complete compilation with FASM

---
+++ src/monochromeCOFF.asm
@@ -0,0 +1,54 @@
+
+include 'include/win64a.inc'
+include 'constants.inc'
+
+
+;;=  Windows
+format MS64 COFF
+;format PE64 NX GUI 6.0
+section '.text' code readable executable
+;;=  Linux
+;format ELF64 executable 3
+;segment readable executable
+
+;entry start
+public main
+public start as '_start'
+
+extrn 'WindowShouldClose' as WindowShouldClose
+start:
+	; Make stack dqword aligned
+	sub	rsp,8
+	sub rsp,$100
+
+.init:
+	;;ProgramInit()
+	call program_init
+
+.loop:
+	;;Update()
+	call update
+	;;Draw()
+	call draw
+	
+	;;While (!WindowShouldClose())
+	call [WindowShouldClose]
+	cmp rax,1
+	jnz .loop
+
+.exit:
+	;;ProgramExit()
+	call program_exit
+	
+	ret
+
+
+
+include 'routines/update.inc'
+include 'routines/draw.inc'
+include 'utilities/program_init.inc'
+include 'utilities/program_exit.inc'
+
+
+include 'data.inc'
+;include 'idata.inc'
